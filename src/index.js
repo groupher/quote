ca131f605e5c69297db5bcf691a5bed20d20bd20
mydearxym <xieym@chinawyny.com>
2019-10-09 16:26:52 +0800

    feat: update style & clean up

--- src/index.js
+++ src/index.js
@@ -12,12 +12,9 @@ require('./index.css').toString();
  * @typedef {object} QuoteData
  * @description Quote Tool`s input and output data
  * @property {string} text - quote`s text
- * @property {'center'|'left'} alignment - quote`s alignment
  *
  * @typedef {object} QuoteConfig
  * @description Quote Tool`s initial configuration
- * @property {string} quotePlaceholder - placeholder to show in quote`s text input
- * @property {'center'|'left'} defaultAlignment - alignment to use as default
  */
 class Quote {
   /**
@@ -29,8 +26,9 @@ class Quote {
    */
   static get toolbox() {
     return {
-      icon: '<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg"><path d="M13.53 6.185l.027.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.569-1.568l4.838-4.837L6.396 2.23A1.125 1.125 0 1 1 7.986.64l5.52 5.518.025.027zm-5.815 0l.026.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.568-1.568l4.837-4.837L.58 2.23A1.125 1.125 0 0 1 2.171.64L7.69 6.158l.025.027z" /></svg>',
-      title: '引用 (Quote)'
+      /* icon: '<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg"><path d="M13.53 6.185l.027.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.569-1.568l4.838-4.837L6.396 2.23A1.125 1.125 0 1 1 7.986.64l5.52 5.518.025.027zm-5.815 0l.026.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.568-1.568l4.837-4.837L.58 2.23A1.125 1.125 0 0 1 2.171.64L7.69 6.158l.025.027z" /></svg>', */
+      icon: '<svg t="1570603614482" class="icon" width="15" height="14" viewBox="0 0 1210 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4264" width="200" height="200"><path d="M517.587974 506.412527a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841665a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497558a47.249136 47.249136 0 0 0 46.547664-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547664-46.547664h-46.497558a362.159864 362.159864 0 0 0-144.302768 29.461815A368.423005 368.423005 0 0 0 29.463819 228.329067 362.310179 362.310179 0 0 0 0.002004 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z m651.366662 0a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841666a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497559a47.249136 47.249136 0 0 0 46.547663-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547663-46.547664h-46.497559a362.159864 362.159864 0 0 0-144.302768 29.461815 368.423005 368.423005 0 0 0-198.466411 198.466411A362.310179 362.310179 0 0 0 651.368666 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z" fill="" p-id="4265"></path></svg>',
+      title: this.i18n === 'en' ? 'Quote' : '引用'
     };
   }
 
@@ -50,39 +48,6 @@ class Quote {
    */
   static get enableLineBreaks() {
     return true;
-  }
-
-  /**
-   * Default placeholder for quote text
-   *
-   * @public
-   * @returns {string}
-   */
-  static get DEFAULT_QUOTE_PLACEHOLDER() {
-    return 'Enter a quote';
-  }
-
-  /**
-   * Allowed quote alignments
-   *
-   * @public
-   * @returns {{left: string, center: string}}
-   */
-  static get ALIGNMENTS() {
-    return {
-      left: 'left',
-      center: 'center'
-    };
-  }
-
-  /**
-   * Default quote alignment
-   *
-   * @public
-   * @returns {string}
-   */
-  static get DEFAULT_ALIGNMENT() {
-    return Quote.ALIGNMENTS.left;
   }
 
   /**
@@ -128,17 +93,11 @@ class Quote {
    *   api - Editor.js API
    */
   constructor({data, config, api}) {
-    const {ALIGNMENTS, DEFAULT_ALIGNMENT} = Quote;
-
     this.api = api;
-
-    this.quotePlaceholder = config.quotePlaceholder || Quote.DEFAULT_QUOTE_PLACEHOLDER;
+    this.i18n = config.i18n || 'en'
 
     this.data = {
       text: data.text || '',
-      alignment: Object.values(ALIGNMENTS).includes(data.alignment) && data.alignment ||
-      config.defaultAlignment ||
-      DEFAULT_ALIGNMENT
     };
   }
 
@@ -154,7 +113,6 @@ class Quote {
       innerHTML: this.data.text
     });
 
-    quote.dataset.placeholder = this.quotePlaceholder;
 
     container.appendChild(quote);
 
@@ -183,7 +141,6 @@ class Quote {
       text: {
         br: true,
       },
-      alignment: {}
     };
   }
 
