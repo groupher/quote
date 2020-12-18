8a5ad85970a03c175184b8a297e1775d7afe6334
mydearxym <mydearxym@qq.com>
2020-12-19 00:08:47 +0800

    style: remove custom-setting class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { make, highlightSettingIcon } from '@groupher/editor-utils'
+import { make, highlightSettingIcon } from "@groupher/editor-utils";
 
 /**
  * Build styles
  */
-import  './index.css'
-import PaperIcon from './icon/paper.svg'
-import QuoteIcon from './icon/quote.svg'
+import "./index.css";
+import PaperIcon from "./icon/paper.svg";
+import QuoteIcon from "./icon/quote.svg";
 
 /**
  * @class Quote
@@ -31,8 +31,9 @@ export default class Quote {
   static get toolbox() {
     return {
       /* icon: '<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg"><path d="M13.53 6.185l.027.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.569-1.568l4.838-4.837L6.396 2.23A1.125 1.125 0 1 1 7.986.64l5.52 5.518.025.027zm-5.815 0l.026.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.568-1.568l4.837-4.837L.58 2.23A1.125 1.125 0 0 1 2.171.64L7.69 6.158l.025.027z" /></svg>', */
-      icon: '<svg t="1570603614482" class="icon" width="15" height="14" viewBox="0 0 1210 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4264" width="200" height="200"><path d="M517.587974 506.412527a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841665a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497558a47.249136 47.249136 0 0 0 46.547664-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547664-46.547664h-46.497558a362.159864 362.159864 0 0 0-144.302768 29.461815A368.423005 368.423005 0 0 0 29.463819 228.329067 362.310179 362.310179 0 0 0 0.002004 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z m651.366662 0a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841666a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497559a47.249136 47.249136 0 0 0 46.547663-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547663-46.547664h-46.497559a362.159864 362.159864 0 0 0-144.302768 29.461815 368.423005 368.423005 0 0 0-198.466411 198.466411A362.310179 362.310179 0 0 0 651.368666 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z" fill="" p-id="4265"></path></svg>',
-      title: this.i18n === 'en' ? 'Quote' : '引用',
+      icon:
+        '<svg t="1570603614482" class="icon" width="15" height="14" viewBox="0 0 1210 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4264" width="200" height="200"><path d="M517.587974 506.412527a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841665a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497558a47.249136 47.249136 0 0 0 46.547664-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547664-46.547664h-46.497558a362.159864 362.159864 0 0 0-144.302768 29.461815A368.423005 368.423005 0 0 0 29.463819 228.329067 362.310179 362.310179 0 0 0 0.002004 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z m651.366662 0a134.482163 134.482163 0 0 0-98.857417-40.735469h-162.841666a69.545917 69.545917 0 0 1-69.796443-69.796443v-23.24878a186.14055 186.14055 0 0 1 186.14055-186.090444h46.497559a47.249136 47.249136 0 0 0 46.547663-46.547664V46.948505a47.249136 47.249136 0 0 0-46.547663-46.547664h-46.497559a362.159864 362.159864 0 0 0-144.302768 29.461815 368.423005 368.423005 0 0 0-198.466411 198.466411A362.310179 362.310179 0 0 0 651.368666 372.631835v511.773776a138.991625 138.991625 0 0 0 139.592886 139.592886h279.135667a138.991625 138.991625 0 0 0 139.592886-139.592886V605.269944a134.482163 134.482163 0 0 0-40.735469-98.857417z" fill="" p-id="4265"></path></svg>',
+      title: this.i18n === "en" ? "Quote" : "引用",
     };
   }
 
@@ -57,20 +58,22 @@ export default class Quote {
   /**
    * Allow Quote to be converted to/from other blocks
    */
-  static get conversionConfig(){
+  static get conversionConfig() {
     return {
       /**
        * To create Quote data from string, simple fill 'text' property
        */
-      import: 'text',
+      import: "text",
       /**
        * To create string from Quote data, concatenate text and caption
        * @param {QuoteData} quoteData
        * @return {string}
        */
       export: function (quoteData) {
-        return quoteData.caption ? `${quoteData.text} — ${quoteData.caption}` : quoteData.text;
-      }
+        return quoteData.caption
+          ? `${quoteData.text} — ${quoteData.caption}`
+          : quoteData.text;
+      },
     };
   }
 
@@ -82,30 +85,30 @@ export default class Quote {
    *   config - user config for Tool
    *   api - Editor.js API
    */
-  constructor({data, config, api}) {
+  constructor({ data, config, api }) {
     this.api = api;
-    this.i18n = config.i18n || 'en'
+    this.i18n = config.i18n || "en";
 
     this.data = {
-      text: data.text || '',
-      type: data.type || 'short', // 'short' || 'part'
-      caption: data.caption || 'someone',
+      text: data.text || "",
+      type: data.type || "short", // 'short' || 'part'
+      caption: data.caption || "someone",
     };
 
     this.settings = [
       {
-        name: 'short',
-        title: '短引用',
+        name: "short",
+        title: "短引用",
         icon: QuoteIcon,
-        type: 'short'
+        type: "short",
       },
       {
-        name: 'paper',
-        title: '长文引用',
+        name: "paper",
+        title: "长文引用",
         icon: PaperIcon,
-        type: 'paper'
+        type: "paper",
       },
-    ]
+    ];
   }
 
   /**
@@ -116,13 +119,12 @@ export default class Quote {
   get CSS() {
     return {
       block: this.api.styles.block,
-      wrapper: 'cdx-quote-paper', // 'cdx-quote',
-      text: 'cdx-quote-papper__text', // 'cdx-quote__text',
+      wrapper: "cdx-quote-paper", // 'cdx-quote',
+      text: "cdx-quote-papper__text", // 'cdx-quote__text',
       input: this.api.styles.input,
 
-      // 
-      quoteSource: 'cdx-quote-source',
-      customSettingWrapper: 'custom-setting-wrapper',
+      //
+      quoteSource: "cdx-quote-source",
 
       quoteSourceLine: "cdx-quote-source__line",
       quoteSourceText: "cdx-quote-source__text",
@@ -138,23 +140,23 @@ export default class Quote {
    * @returns {Element}
    */
   render() {
-    const Wrapper = make('div', [this.CSS.block]);
-    const Blockquote = make('blockquote', [this.CSS.wrapper]);
+    const Wrapper = make("div", [this.CSS.block]);
+    const Blockquote = make("blockquote", [this.CSS.wrapper]);
 
-    const Quote = make('div', [this.CSS.input, this.CSS.text], {
+    const Quote = make("div", [this.CSS.input, this.CSS.text], {
       innerText: this.data.text || "",
       contentEditable: true,
       placeholder: "引用内容。",
     });
 
     // bottom source
-    const QuoteSource = make('div', [this.CSS.quoteSource])
-    const QuoteSourceLine = make('div', [this.CSS.quoteSourceLine])
-    const QuoteSourceText = make('div', [this.CSS.quoteSourceText], {
+    const QuoteSource = make("div", [this.CSS.quoteSource]);
+    const QuoteSourceLine = make("div", [this.CSS.quoteSourceLine]);
+    const QuoteSourceText = make("div", [this.CSS.quoteSourceText], {
       contentEditable: true,
-      innerText: '',
+      innerText: "",
       placeholder: "引用来源",
-    })
+    });
 
     QuoteSource.appendChild(QuoteSourceLine);
     QuoteSource.appendChild(QuoteSourceText);
@@ -173,25 +175,25 @@ export default class Quote {
    * @returns {Element}
    */
   renderSettings() {
-    const Wrapper = make('div', [this.CSS.customSettingWrapper])
+    const Wrapper = make("div");
 
-    this.settings.forEach( (item) => {
-      const itemEl = make('div', [this.CSS.settingsButton], {
-        innerHTML: item.icon
+    this.settings.forEach((item) => {
+      const itemEl = make("div", [this.CSS.settingsButton], {
+        innerHTML: item.icon,
       });
 
-      if (this.data.type === item.name) highlightSettingIcon(itemEl, this.api)
+      if (this.data.type === item.name) highlightSettingIcon(itemEl, this.api);
 
-      this.api.tooltip.onHover(itemEl, item.title, { placement: 'top' });
-      itemEl.addEventListener('click', () => {
+      this.api.tooltip.onHover(itemEl, item.title, { placement: "top" });
+      itemEl.addEventListener("click", () => {
         // this.setCenterIcon(item.name);
-        highlightSettingIcon(itemEl, this.api)
+        highlightSettingIcon(itemEl, this.api);
       });
 
       Wrapper.appendChild(itemEl);
-    })
-    
-    return Wrapper
+    });
+
+    return Wrapper;
   }
 
   /**
